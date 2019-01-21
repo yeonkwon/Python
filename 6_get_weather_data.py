ded5c98d1ba8865ffdabb69f4442433b828d8c6d
yeonkwon <ukyle@naver.com>
2019-01-21 15:42:57 +0900

    new0121

--- 6_get_weather_data.py
+++ 6_get_weather_data.py
@@ -4,7 +4,7 @@ with open('./SeoulKR.json') as data_file:
         data = json.load(data_file)
         print(data['main'])
 
-        toCelsius = data['main']['temp']    273
+        toCelsius = data['main']['temp'] + 273
         weather = data['weather'][0]['main']
         print(toCelsius)
         print(weather)
---
+++ weather.py
@@ -0,0 +1,10 @@
+import json
+
+with open('./SeoulKR.json') as data_file:
+	data = json.load(data_file)
+	print(data['main'])
+
+	toCelsius = data['main']['temp'] + 273
+	weather = data['weather'][0]['main']
+	print(toCelsius)
+	print(weather)
